54a1d56234412662dfbf38c5e497db46785827f5
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-12 22:21:00 +0100

    Add task-01 and task-02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,4 +1,17 @@
-const categories = document.querySelectorAll('#categories .item');
-console.log('Number of categories:', categories.length);
+'use strict'
+const categoriesList = document.getElementById('categories');
+const categories = categoriesList.querySelectorAll('li.item');
+const numberOfCategories = categories.length;
+console.log('Number of categories:', numberOfCategories);
+categories.forEach(category => {
+    const categoryName = category.querySelector('h2').textContent;
+    const categoryElements = category.querySelectorAll('ul > li');
+    const numberOfElements = categoryElements.length;
+    console.log('Category:', categoryName);
+    console.log('Elements:', numberOfElements);
+});
+
+
+
 
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  ingredientsList.appendChild(listItem);
+});
+
