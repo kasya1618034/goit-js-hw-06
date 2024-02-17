9561396a87d2e23582231b090be92a59ebf01e92
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-17 18:48:38 +0100

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
+    const formData = {};
+
+    for (const element of form.elements) {
+        if (element.type !== "submit") {
+            formData[element.name] = element.value;
+        }
+    }
+
+    if (!formData.email || !formData.password) {
+        alert("Fill in all fields, please!");
+        return;
+    }
+
+   
+    console.log("Form Data:", formData);
+
+    form.reset();
+});
